6df41bc288e4767c755724386cafd200c612773f
kannan_b <bkannan282@gmail.com>
2023-07-14 07:37:55 +0530

    Created new flows

--- src/test/java/samples/SampleTest.java
+++ src/test/java/samples/SampleTest.java
@@ -8,10 +8,12 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
-public class SampleTest {
+class SampleTest {
 
 	public static void listexamples() {
 		int[] array = { 6, 1, 2, 3, 3, 4, 5, 6, 5, 5, 6 };
@@ -144,6 +146,9 @@ public class SampleTest {
 		for (int j = 0; j < newarrays.length; j++) {
 			System.out.print(newarrays[j] + " ");
 		}
+		
+		HashSet<Integer> as = new HashSet<>();
+		
 
 	}
 
@@ -294,6 +299,16 @@ public class SampleTest {
 
 		System.out.println();
 		int n = arr.length;
+		
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = 0; j < arr.length-i-1; j++) {
+				int temp = arr[j];
+				arr[j]= arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+		
+		
 
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = 0; j < n - i - 1; j++) {
@@ -315,9 +330,14 @@ public class SampleTest {
 	public static void lengthOfLastWord() {
 		String s1 = "luffy is still joboy   ";
 
+		System.out.println(s1.substring(2, 4));
 		String lastword[] = s1.split(" ");
 		System.out.println(lastword[lastword.length - 1].length());
-
+		
+		int a = 11;
+		s1 =String.valueOf(a);
+		System.out.println(s1.repeat(50));
+		System.out.println(s1.indent(5));
 	}
 
 	
@@ -329,9 +349,488 @@ public class SampleTest {
 			
 		}
 	}
-	public static void main(String[] args) {
-		loopWorks();
+	
+	static void findLargestInArray() {
+		int[] array = {1,2,3,5,6,3,62,97,4};
+		int small = -1;
+		int large = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if(array[i]>large) {
+				large=array[i];
+				small = large;
+			} 
+			
+			else if(array[i]>small) {
+				small = array[i];
+			}
+		}
+		
+		System.out.println(large+" "+small);
+		
+		
+		int[] numbers = {1,2,3,5,6,3,62, 97, 97,4};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
 
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                secondLargest = largest;
+                largest = numbers[i];
+            } else if (numbers[i] > secondLargest && numbers[i] != largest) {
+                secondLargest = numbers[i];
+            }
+        }
+
+        
+       
+        
+        if (secondLargest != Integer.MIN_VALUE) {
+            System.out.println("The second largest number is: " + secondLargest);
+        } else {
+            System.out.println("No second largest number found.");
+        }
+    }
+
+	
+	static void stringReverseWithoutInbuild() {
+		String toReverse = "hello world";
+		String reverse = "";
+		
+		for (int i = toReverse.toCharArray().length-1; i >= 0; i--) {
+			reverse = reverse + toReverse.charAt(i);
+		}
+		System.out.println(reverse);
+	}
+	
+	
+	static void countNoOfWords() {
+		String toReverse = "hello world is the new hello world";
+		
+		HashMap<Integer, Integer> hashMap = new HashMap<>();
+		
+		int count  = toReverse.split(" ").length;
+		
+	}
+	
+	static void secondLargest() {
+		        int arr[] = { 100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89, 100 };
+		        int largest = 0;
+		        int secondLargest = 0;
+		        System.out.println("The given array is:");
+		        for (int i = 0; i < arr.length; i++)
+		        {
+		            System.out.print(arr[i] + "\t");
+		        }
+		        for (int i = 0; i < arr.length; i++)
+		        {
+		            if (arr[i] > largest)
+		            {
+		                secondLargest = largest;
+		                largest = arr[i];
+		            }
+		            else if (arr[i] > secondLargest && arr[i]!=largest)
+		            {
+		                secondLargest = arr[i];
+		            }
+		        }
+		        System.out.println("\nSecond largest number is:" + secondLargest);
+		        System.out.println("Largest Number is: "  +largest);
+		    
+	}
+	
+	static void maxOccurences() {
+	    String str = "helloworld";
+
+	    // Create a character array to store the frequency of each character in the string
+	    int[] charCount = new int[256];
+
+	    // Iterate through the string and update the frequency of each character
+	    for (int i = 0; i < str.length(); i++) {
+	      char currentChar = str.charAt(i);
+	      charCount[currentChar]++;
+	    }
+
+	    // Find the maximum and minimum occurring characters
+	    char maxChar = ' ';
+	    int maxCount = -1;
+	    char minChar = ' ';
+	    int minCount = Integer.MAX_VALUE;
+
+	    for (int i = 0; i < charCount.length; i++) {
+	      if (charCount[i] > maxCount) {
+	        maxChar = (char) i;
+	        maxCount = charCount[i];
+	      } else if (charCount[i] < minCount) {
+	        minChar = (char) i;
+	        minCount = charCount[i];
+	      }
+	    }
+
+	    // Print the maximum and minimum occurring characters
+	    System.out.println("The maximum occurring character in the string is: " + maxChar);
+	    System.out.println("The minimum occurring character in the string is: " + minChar);
+
+	}
+
+	static void secondLargestFinding() {
+		int[] array = {1,2,3,4,5,1,434,312,3421,123,312,3421};
+		int max = array[0];
+		int small = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < array.length; i++) {
+			if(array[i]>max) {
+				small = max;
+				max = array[i];
+				
+				
+			} else if(array[i]>small && array[i]!=max) {
+				small = array[i];
+				
+			}
+		}
+		
+		System.out.println(small);
+		
+	}
+	
+	static void findCharacterCount() {
+		String java = "Testing is good !";
+		
+		char c;
+
+	    for(c = 'a'; c <= 'z'; ++c) {
+	    
+	    	java = java.toLowerCase();
+	    	int count = java.length()-java.replaceAll(String.valueOf(c), "").length();
+	    	if(count>0) {
+	    		System.out.println(c+" "+count);
+	    	}
+	    	
+	    }
+	}
+	
+	static void findDuplicateCharactersUsingHashMpa() {
+		
+		String java = "Testing is good !";
+		char[] array = java.toCharArray();
+		
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		int count = 1;
+		
+		for (int i = 0; i < array.length; i++) {
+			
+			if(!hm.containsKey(array[i])) {
+				hm.put(array[i], count);
+			} else {
+				hm.put(array[i], hm.get(array[i])+1);
+			}
+			
+		}
+		
+		for(Map.Entry<Character, Integer> forloop : hm.entrySet()) {
+			char key = forloop.getKey();
+			int value = forloop.getValue();
+			
+			if(value>1) {
+				System.out.println(key+ " "+value);
+			}
+		}
+		
+	}
+	
+	static void reverseWordsInSentence() {
+		String sen = "hey this is fund to do";
+		String[] array = sen.split(" ");
+		String temp = "";
+		for(String a:array) {
+			
+			for(int i=a.toCharArray().length-1 ; i>=0;i--) {
+				temp = temp+a.charAt(i);
+			}
+			temp = temp+" ";
+			
+		}
+		System.out.println(temp);
+	}
+	
+	static void LargestAndSmallestInArray() {
+		
+	     int arr[] = { 100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89, 100 };
+	     int large = arr[0];
+	     int small = arr[0];
+	     
+	     for (int i = 0; i < arr.length; i++) {
+	    	 
+	    	 if(arr[i] > large) {
+	    		 large = arr[i];
+	    	 } else if(arr[i] < small) {
+	    		 small = arr[i];
+	    	 }
+			
+		}
+	     
+	     System.out.println(large+" "+small);
+	     
+	     TreeSet<Integer> ts = new TreeSet<>();
+	     for(int a: arr) {
+	    	 ts.add(a);
+	     }
+
+	     System.out.println(ts.first()+" "+ts.last());
+	     System.out.println(ts.descendingSet());
+	     
+	     int i =0;
+	     for(Integer number : ts){
+	    	 
+	    	 if(i==0 || i==ts.size()-1) {
+		    		System.out.println(number); 
+		    	 }
+	    	 i++;
+	    	 
+	        }  
+	}
+	
+	static void SortArrayWithForLoop() {
+		
+	     int arr[] = { 100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89, 100 };
+	     
+	     List<int[]> l = Arrays.asList(arr);
+	     
+	    
+	     
+	     int count = 0;
+	     
+	    for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				
+				if(arr[i] < arr[j]) {
+					count = arr[i];
+					arr[i] = arr[j];
+					arr[j] = count;
+				
+			}
+			
+		}
+	    }
+	    for (int i1 : arr) {
+	    	System.out.println(i1);
+		}
+	}
+	
+	
+	static void reverseNumber () {
+		int num = 6598;
+		int result = 0;
+		while(num>0) {
+			result = result*10 + num%10;
+			num = num/10;
+		}
+		
+		
+		System.out.println(result);
+	}
+	
+	static void fibo() {
+		int a = 0;
+		int b =1;
+		int c = 0;
+		for (int i = 0; i < 12; i++) {
+			 c = a + b;
+			System.out.println(c+" ");
+			a = b;
+			b = c;
+		}
+		
+		System.out.println(c);
+	}
+	
+	static int factRecursive(int n) {
+		
+		if(n<=1) {
+			return 1;
+		} else {
+			return n*factRecursive(n-1);
+		}
+	}
+	
+	static void printPattern() {
+		int count = 1;
+		for (int i = 1; i < 5; i++) {
+			for (int j = 5; j >i ; j--) {
+				System.out.print(count+" ");
+				count++;
+			}
+			System.out.println();
+		}
+		
+		for (int i = 1; i < 5; i++) {
+			for (int j = 5; j >i ; j--) {
+				System.out.print("*"+" ");
+				count++;
+			}
+			System.out.println();
+		}
+		
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j <=i ; j++) {
+				System.out.print("*"+" ");
+				count++;
+			}
+			System.out.println();
+		}
+		
+		
+		//x for rows, y for columns, and row denotes the number of rows to print
+		int x, y, row = 5; 
+		//Outer loop for rows 
+		for (x=0; x<row; x++) 
+		{ 
+		//inner loop for space 
+		for (y=row-x; y>1; y--) 
+		{ 
+		//To print space between two stars 
+		System.out.print(" "); 
+		} 
+		//inner loop for columns 
+		for (y=0; y<=x; y++ ) 
+		{ 
+		//To print star 
+		System.out.print(y+" "); 
+		} 
+		//Cursor goes to the new line after printing each line.
+		System.out.println(); 
+		}
+		
+		
+		
+		for (int i = 1; i <= 6; i++) {
+			
+			for (int j = 1; j <=6-i; j++) {
+				System.out.print("");
+			}
+			
+			for (int k = 1; k <= i; k++) {
+				System.out.print("& ");				
+			}
+			
+			
+			System.out.println();
+		}
+		
+		int rows =6;
+		// Loop for the number of rows
+        for (int i = 1; i <= rows; i++) {
+            // Loop to print the spaces before the asterisks
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+
+            // Loop to print the asterisks in each row
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println(); // Move to the next line
+        }
+		
+	}
+	
+	static void swapNumber() {
+		int a = 10;
+		int b = 20;
+		
+		a = a +b; // 10+20 = 30
+		b = a-b; // 30-20 = -10
+		a =a-b; // 
+		
+		System.out.println(a+" "+b);
+	}
+	
+	static void printPrimeNumber () {
+		
+		for (int i = 0; i < 20; i++) {
+			for (int j = 2; j < Math.sqrt(i); j++) {
+				if(i%j==0) {
+					System.out.println(i+" not a prime");
+				} else {
+					System.out.println(i+" is a prime");
+				}
+				
+			}
+		}
+	}
+	
+	
+	static void duplicatepractice() {
+		int arr[] = { 100,14, 46, 47, 94, 94, 52, 86, 36, 94, 89, 100 };
+		
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if(!hm.containsKey(arr[i])) {
+				hm.put(arr[i], 1);
+			} else {
+				hm.put(arr[i], hm.get(arr[i])+1);
+			}
+			
+		}
+		
+		
+		
+		for(Entry<Integer, Integer> m : hm.entrySet()) {
+			int key = m.getKey();
+			int value = m.getValue();
+			if(value==1) {
+				System.out.println(key);
+				
+				
+			}
+		}
+		
+	}
+	
+	static void duplicatesRemoveUsingLoop() {
+		
+        int[] array = {1, 1, 2, 3, 3, 4, 5, 6, 6, 7};
+        if (array.length == 0) {
+           // return array; // Empty array, no duplicates to remove
+        }
+
+        int uniqueCount = 1; // Count of unique elements (initially set to 1)
+        
+        // Check each element starting from the second element
+        for (int i = 1; i < array.length; i++) {
+            boolean isDuplicate = false;
+            
+            // Check if the current element is a duplicate of any previous element
+            for (int j = 0; j < uniqueCount; j++) {
+                if (array[i] == array[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            
+            // If the current element is not a duplicate, add it to the unique elements
+            if (!isDuplicate) {
+                array[uniqueCount] = array[i];
+                uniqueCount++;
+            }
+            
+        }
+        Arrays.copyOf(array, uniqueCount);
+
+       // System.out.println(Arrays.toString(uniqueCount));
+
+        
+        
+
+	}
+	public static void main(String[] args) {
+		//findCharacterCount();
+		duplicatesRemoveUsingLoop();
+		
 	}
 
 }
